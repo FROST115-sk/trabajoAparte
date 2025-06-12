c43d5fb06aba688211dce1631e772c1178eb625f
MOR-S115 <stevenmartinez182024@gmail.com>
2025-06-12 13:48:26 -0600

    se volvio a multiplicar el archivo.pi

--- as-note.py
+++ as-note.py
@@ -1,5 +1,5 @@
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
-asignaturas=["matematica","fisica","lenguaje","quimica","historia"]historia
+asignaturas=["matematica","fisica","lenguaje","quimica","historia"]historia,biologia
 notes={}
 for asignatura in asignaturas:
     nota=float(input("ingresa la nota que has sacado en " + asignatura +":"))
